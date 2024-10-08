1e0d56f50754c07d1577f7560c51f9e0d4a00895
Matias <matiasredruello@gmail.com>
2024-10-08 03:32:33 +0000

    Add:archivo git history VM2

--- RTA_SCRIPT_Examen_20241006/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_B.sh
@@ -6,36 +6,46 @@ e
 1
 
 
-n 
-
-+1G
 n
 
-+1G
++500M
 n
 
-+1G
++500M
 n
 
-+1G
++500M
 n
 
-+1G
++500M
 n
 
-+1G
++500M
 n
 
-+1G
++500M
 n
 
-+1G
++500M
 n
 
-+1G
++500M
+n
+
++500M
 n
 
 
 p
 w
 EOF
+sudo mkfs.ext4 /dev/sdc5
+sudo mkfs.ext4 /dev/sdc6
+sudo mkfs.ext4 /dev/sdc7
+sudo mkfs.ext4 /dev/sdc8
+sudo mkfs.ext4 /dev/sdc9
+sudo mkfs.ext4 /dev/sdc10
+sudo mkfs.ext4 /dev/sdc11
+sudo mkfs.ext4 /dev/sdc12
+sudo mkfs.ext4 /dev/sdc13
+sudo mkfs.ext4 /dev/sdc14
