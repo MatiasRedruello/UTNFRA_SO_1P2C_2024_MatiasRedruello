b36270bd96fd202e2a8e7cb00d832c3f46f2ecec
Matias Redruello <matiasredruello@gmail.com>
2024-10-09 23:42:31 +0000

    fix: punto D

--- RTA_SCRIPT_Examen_20241006/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -1,10 +1,7 @@
-i#!/bin/bash
+#!/bin/bash
 
 sudo mkdir -p /Estructura_asimetrica/{correo/{carteros_{1..10},cartas_{1..100}},cliente/cartas_{1..100}}
-
 cd /
-
-cd Estructura_Asimetrica/
-
-sudo tree --noreport | pr -T -s' ' -w 80 --column 4
+cd Estructura_asimetrica/
+tree --noreport | pr -T -s' ' -w 80 --column 4
 
