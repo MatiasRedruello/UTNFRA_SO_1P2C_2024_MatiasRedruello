8a1f3259973cc7d4cee4683fe39b92e7f8ef1b07
matias redruello <matiasredruello@gmail.com>
2024-10-09 15:19:24 +0000

    Fix: Arreglo puto D cleintes--->cliente

--- RTA_SCRIPT_Examen_20241006/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+i#!/bin/bash
 
-sudo mkdir -p /Estructura_asimetrica/{correo/{carteros_{1..10},cartas_{1..100}},clientes/cartas_{1..100}}
+sudo mkdir -p /Estructura_asimetrica/{correo/{carteros_{1..10},cartas_{1..100}},cliente/cartas_{1..100}}
 
 cd /
 
